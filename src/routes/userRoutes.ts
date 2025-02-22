0ff523234dab542aa813f5abe0e90c64037ccdfc
Star <snabajja@gmail.com>
2025-02-22 14:33:21 -0500

    Add export statement

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,3 +5,5 @@ const router = Router();
 router.route('/').get(getUsers).post(createUser);
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 router.route('/:id/friends/:friendId').post(addFriend).delete(removeFriend);
+
+export default router;
