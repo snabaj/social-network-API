89c4c706c842b30868deeb259a3f84b577fd755e
Star <snabajja@gmail.com>
2025-02-22 16:16:45 -0500

    Refactor userRoutes to include "/api" in the route paths

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -10,8 +10,8 @@ import {
 } from "../controllers/userController.js";
 
 const router = Router();
-router.route("/").get(getUsers).post(createUser);
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
-router.route("/:id/friends/:friendId").post(addFriend).delete(removeFriend);
+router.route("/api/users").get(getUsers).post(createUser);
+router.route("/api/users/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/api/users/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 export default router;
